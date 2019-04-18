4918e0836bdadbaac2de543460574d04b5cdd04d
Satoriq <sudo.satoriq@gmail.com>
2019-04-18 13:02:09 +0300

    fix: remove redux devtool
    fix: remove redux devtool

--- src/index.js
+++ src/index.js
@@ -7,14 +7,7 @@ import rootReducer from './Store/reducers/rootReducer';
 import './index.css';
 import App from './App';
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-);
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
