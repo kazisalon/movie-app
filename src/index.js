4936904db0afc100cd2776770d434d5c9f92b479
Satoriq <sudo.satoriq@gmail.com>
2019-04-22 10:30:19 +0300

    fix: scroll-behavior for firefox, another method
    fix: scroll-behavior for firefox, another method

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ import thunk from 'redux-thunk';
 import 'ie-array-find-polyfill';
 import 'promise-polyfill/src/polyfill';
 import 'whatwg-fetch';
-import smoothscroll from 'smoothscroll-polyfill';
 import rootReducer from './Store/reducers/rootReducer';
 import './index.css';
 import App from './App';
 
 
 // kick off the polyfill!
-smoothscroll.polyfill();
 
 
 const store = createStore(
